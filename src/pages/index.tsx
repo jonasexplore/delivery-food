29479680f8fccee9daaa7760f2404884d6d080e4
Jonas <jonasexplore@gmail.com>
2022-04-25 20:58:44 -0300

    feat: added buttons to signup and signin pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import { Typography } from "antd";
 import { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Typography.Title>Bem vindo o delivery food</Typography.Title>
+      <Typography.Paragraph>
+        Para prosseguir, escolha uma das opções abaixo.
+      </Typography.Paragraph>
+      <Link href="/auth/signin">Entrar</Link>
+      <Link href="/auth/signup">Criar uma conta</Link>
+    </div>
+  );
 };
 
 export default Home;
