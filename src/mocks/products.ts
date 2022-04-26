dfd3dbd2aa1501f9df84af79b8acfb69c0c6a99d
Jonas <jonasexplore@gmail.com>
2022-04-26 11:48:33 -0300

    chore(mocks): added product mock

---
+++ src/mocks/products.ts
@@ -0,0 +1,52 @@
+import frontCover from "../assets/images/front-cover.jpg";
+
+export const products = [
+  {
+    id: "1",
+    frontCover,
+    company: "Royal Sushi House",
+    deliveryTime: "30-40 min",
+    categories: [{ id: "1", category: "Sushi" }],
+  },
+  {
+    id: "2",
+    frontCover,
+    company: "Burgers & Pizza",
+    deliveryTime: "40-60 min",
+    categories: [
+      { id: "2", category: "Burger" },
+      { id: "3", category: "Pizza" },
+    ],
+  },
+  {
+    id: "3",
+    frontCover,
+    company: "Ninja Sushi",
+    deliveryTime: "20-40 min",
+    categories: [{ id: "1", category: "Sushi" }],
+  },
+  {
+    id: "4",
+    frontCover,
+    company: "Royal Sushi House",
+    deliveryTime: "30-40 min",
+    categories: [{ id: "1", category: "Sushi" }],
+  },
+  {
+    id: "5",
+    frontCover,
+    company: "Burgers & Pizza",
+    deliveryTime: "40-60 min",
+    categories: [
+      { id: "2", category: "Burger" },
+      { id: "3", category: "Pizza" },
+    ],
+  },
+  {
+    id: "6",
+    frontCover,
+    company: "Ninja Sushi",
+    deliveryTime: "20-40 min",
+    categories: [{ id: "1", category: "Sushi" }],
+  },
+];
