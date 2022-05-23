c312fe64b41092992e3e370f6e91522c71937301
Jonas <jonasexplore@gmail.com>
2022-05-23 16:05:45 -0300

    chore: added deals mock

---
+++ src/mocks/deals.ts
@@ -0,0 +1,13 @@
+import deal1 from "../assets/images/deals/1.jpg";
+import deal2 from "../assets/images/deals/2.jpg";
+
+export const deals = [
+  {
+    id: "1",
+    image: deal1,
+  },
+  {
+    id: "2",
+    image: deal2,
+  },
+];
